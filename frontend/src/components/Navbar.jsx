11abc9d4b4da5dd6f78462c301110996f6ad1b7f
gurucharan <Gurucharan3422>
2025-03-27 01:15:37 -0700

    fixed navbar.css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './navbar.css';
+import './Ngitavbar.css';
 
 const Navbar = () => {
   const navigate = useNavigate();
